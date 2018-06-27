426a8f37a8de3ba803bf3e19d8f612a8ef2856a1
Ollie Hughes <olli.hughes@gmail.com>
2018-06-27 10:11:51 +0100

    Initial commit from start.spring.io

---
+++ src/main/java/hackday/southampton/eurek8s/Eurek8sApplication.java
@@ -0,0 +1,12 @@
+package hackday.southampton.eurek8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eurek8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eurek8sApplication.class, args);
+	}
+}
